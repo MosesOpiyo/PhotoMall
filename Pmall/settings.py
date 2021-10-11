409505481647008c1117864848f78c52bd26730b
Moses <moses.junior@student.moringaschool.com>
2021-10-11 10:53:09 +0300

    connecting database

--- Pmall/settings.py
+++ Pmall/settings.py
@@ -77,7 +77,9 @@ WSGI_APPLICATION = 'Pmall.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'photomall',
+        'USER': 'moses',
+        'PASSWORD':'1234',
     }
 }
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
